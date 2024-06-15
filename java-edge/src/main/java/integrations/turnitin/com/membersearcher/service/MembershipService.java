e6206bafb26c9f4eadd41df528381544cbd25af6
Eric Preston <eric.preston@redis.com>
2024-06-15 16:59:26 -0500

    Call fetchUsers once instead of fetchUser for each membership

--- java-edge/src/main/java/integrations/turnitin/com/membersearcher/service/MembershipService.java
+++ java-edge/src/main/java/integrations/turnitin/com/membersearcher/service/MembershipService.java
@@ -5,6 +5,7 @@ import java.util.concurrent.CompletableFuture;
 import integrations.turnitin.com.membersearcher.client.MembershipBackendClient;
 import integrations.turnitin.com.membersearcher.model.MembershipList;
 
+import integrations.turnitin.com.membersearcher.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,21 +16,26 @@ public class MembershipService {
 
 	/**
 	 * Method to fetch all memberships with their associated user details included.
-	 * This method calls out to the php-backend service and fetches all memberships,
-	 * it then calls to fetch the user details for each user individually and
-	 * associates them with their corresponding membership.
+	 * This method calls out to the php-backend service and fetches all users,
+	 * then all memberships.
+	 * For each member it finds the corresponding user from the users list and sets the details.
 	 *
 	 * @return A CompletableFuture containing a fully populated MembershipList object.
 	 */
 	public CompletableFuture<MembershipList> fetchAllMembershipsWithUsers() {
-		return membershipBackendClient.fetchMemberships()
-				.thenCompose(members -> {
-					CompletableFuture<?>[] userCalls = members.getMemberships().stream()
-							.map(member -> membershipBackendClient.fetchUser(member.getUserId())
-									.thenApply(member::setUser))
-							.toArray(CompletableFuture<?>[]::new);
-					return CompletableFuture.allOf(userCalls)
-							.thenApply(nil -> members);
-				});
+        return membershipBackendClient.fetchUsers()
+                .thenCompose(users -> membershipBackendClient.fetchMemberships()
+                    .thenCompose(members -> {
+                        CompletableFuture<?>[] userCalls = members.getMemberships().stream()
+                                .map(member -> {
+									User matchingUser = users.getUsers().stream().filter(u ->
+											u.getId().equals(member.getUserId())).findAny().orElse(null);
+									member.setUser(matchingUser);
+									return CompletableFuture.completedFuture(member);
+								})
+                                .toArray(CompletableFuture<?>[]::new);
+                        return CompletableFuture.allOf(userCalls)
+                                .thenApply(nil -> members);
+				}));
 	}
 }
--- java-edge/src/test/java/integrations/turnitin/com/membersearcher/MembershipServiceTest.java
+++ java-edge/src/test/java/integrations/turnitin/com/membersearcher/MembershipServiceTest.java
@@ -8,6 +8,7 @@ import integrations.turnitin.com.membersearcher.client.MembershipBackendClient;
 import integrations.turnitin.com.membersearcher.model.Membership;
 import integrations.turnitin.com.membersearcher.model.MembershipList;
 import integrations.turnitin.com.membersearcher.model.User;
+import integrations.turnitin.com.membersearcher.model.UserList;
 import integrations.turnitin.com.membersearcher.service.MembershipService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -28,13 +29,13 @@ public class MembershipServiceTest {
 	private MembershipBackendClient membershipBackendClient;
 
 	@Mock
-	private ObjectMapper objectMapper;
-
 	private MembershipList members;
 
 	private User userOne;
 
 	private User userTwo;
+
+	private UserList users;
 
 	@BeforeEach
 	public void init() {
@@ -57,14 +58,15 @@ public class MembershipServiceTest {
 				.setId("2")
 				.setName("test two")
 				.setEmail("test2@example.com");
+		users = new UserList()
+				.setUsers(List.of(userOne, userTwo));
+
 		when(membershipBackendClient.fetchMemberships()).thenReturn(CompletableFuture.completedFuture(members));
-		when(membershipBackendClient.fetchUser("1")).thenReturn(CompletableFuture.completedFuture(userOne));
-		when(membershipBackendClient.fetchUser("2")).thenReturn(CompletableFuture.completedFuture(userTwo));
+		when(membershipBackendClient.fetchUsers()).thenReturn(CompletableFuture.completedFuture(users));
 	}
 
 	@Test
 	public void TestFetchAllMemberships() throws Exception {
-
 		MembershipList members = membershipService.fetchAllMembershipsWithUsers().get();
 		assertThat(members.getMemberships().get(0).getUser()).isEqualTo(userOne);
 		assertThat(members.getMemberships().get(1).getUser()).isEqualTo(userTwo);
